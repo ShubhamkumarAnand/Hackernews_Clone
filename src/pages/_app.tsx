a07375fae9dbbf3c54a08aa0ce5801a11386cbf8
imskanand <the.anand3099@gmail.com>
2023-03-16 13:22:15 +0530

    :heavy_plus_sign: Added Apollo and graphql :package: change config file :tada: Configured Apollo client to the next app :bookmark: 0.0.1 :books: Update Docs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
 import { type AppType } from "next/dist/shared/lib/utils";
+import { client } from "~/lib/apollo/apolloClient";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />;
+      </ApolloProvider>
+    </>
+  );
 };
 
 export default MyApp;
